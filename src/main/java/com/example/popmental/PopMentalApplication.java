08a097c8d8619105b370d1cda4e699b56885e767
nicol <nicolaskraus.email@gmail.com>
2023-03-16 10:16:48 +0100

    init commit

---
+++ src/main/java/com/example/popmental/PopMentalApplication.java
@@ -0,0 +1,13 @@
+package com.example.popmental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopMentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopMentalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/popmental/PopMentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.popmental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopMentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
